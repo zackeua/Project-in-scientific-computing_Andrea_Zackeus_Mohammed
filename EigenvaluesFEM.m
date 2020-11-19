b34b8a11aa171fa5c02e75c79e4e226b9d79cfb7
Mohammed-Mosa <mohammedmosa@windowslive.com>
2020-11-19 23:06:50 +0100

    uppdate_analecic_to_analytic

--- EigenvaluesFEM.m
+++ EigenvaluesFEM.m
@@ -12,12 +12,12 @@ h = (right-left)/(m);
 
 u_0 = 1; % amplitude?
 k = 2*pi*2; % wave speed?
-analycic = @(x,t) real(u_0*exp(1i*k*(x-t))); % might be the analythical solution
+analytic = @(x,t) real(u_0*exp(1i*k*(x-t))); % might be the analythical solution
 
 
 for i = 1:m
     x(i) = h*(i-1);
-    u0(i) = analycic(x(i),0);
+    u0(i) = analytic(x(i),0);
 end
 u1 = u0;
 
@@ -63,7 +63,7 @@ for degree = 1:6
     dt = dtmax*0.9/2;
     T = 0;
     while T < 1
-       g1 = dt * RK * u1; %% minus är inlaggt i RK = -M\(L+a*K)
+       g1 = dt * RK * u1; %% minus ??r inlaggt i RK = -M\(L+a*K)
        g2 = dt * RK * (u1 + g1/2);
        g3 = dt * RK * (u1 + g2/2);
        g4 = dt * RK * (u1 + g3);
@@ -75,7 +75,7 @@ for degree = 1:6
     figure;
     %plot(x,analycic(x,1),x,u1,x,u2,x,u3,x,u4,x,u5(:,2),x,u6(:,2));
     %legend('analytic','RK4 - h^2','Euler - h^2','RK4 - h^4','Euler - h^4','leapfrog - h^2','leapfrog - h^4',"Location","best");
-    plot(x,analycic(x,1),x,u1);
+    plot(x,analytic(x,1),x,u1);
     legend('analytic','RK4',"Location","best");
     title(['T = ', num2str(T), ', with order: ', num2str(degree), ' and timestep: ', num2str(dt)])
     e = analytic(x,T) - u1;
