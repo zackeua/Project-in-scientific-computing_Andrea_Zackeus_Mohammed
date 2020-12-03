a559926676f7d6856560abc0723d5943251eb043
Zackeus Zetterberg <zackeua@gmail.com>
2020-12-03 16:29:00 +0100

    plottat för FDM

--- C_effPlot.m
+++ C_effPlot.m
@@ -9,14 +9,17 @@ C_gaussLobatto = C_eff;
 load('gaussLobattoExact');
 C_gaussLobattoExact = C_eff;
 
+load('fdm');
+
 
 figure
 plot(C_uni,'-*');
 hold on;
 plot(C_gaussLobatto,'-*');
 plot(C_gaussLobattoExact,'-*');
+plot(2*(1:4),CFL,'-*');
 hold off;
-legend('Uniform', 'Gauss Lobatto', 'Gauss Lobatto exact integration');
+legend('Uniform', 'Gauss Lobatto', 'Gauss Lobatto exact integration', 'FDM');
 xlabel('Degree')
 ylabel('C_{eff}')
 title('Rescaled efficiency number as a function of the polynomial degree');
@@ -27,8 +30,9 @@ plot(1./C_uni,'-*');
 hold on;
 plot(1./C_gaussLobatto,'-*');
 plot(1./C_gaussLobattoExact,'-*');
+plot(2*(1:4),1./CFL,'-*');
 hold off;
-legend('Uniform', 'Gauss Lobatto', 'Gauss Lobatto exact integration');
+legend('Uniform', 'Gauss Lobatto', 'Gauss Lobatto exact integration', 'FDM');
 xlabel('Degree')
 ylabel('1/C_{eff}')
 title('Rescaled efficiency number as a function of the polynomial degree');
---
+++ FDMplot.m
@@ -0,0 +1,28 @@
+clear;
+close all;
+clc;
+ps = 2*(1:4);
+CFL = [];
+for degree = ps
+    w = weights(degree,1);
+    u = -degree/2:degree/2;
+
+    deg = 0:0.01:2*pi;
+    t = [];
+    for angle = deg
+        t = [t, abs(w*exp(1i*u*angle)')];
+    end
+    
+    CFL = [CFL, sqrt(8)/max(t)];
+
+    
+    plot(deg, t);
+    title('|t(\xi)|')
+    xlabel('\xi');
+    ylabel('|t(\xi)|')
+    
+    disp(['CFL = ' num2str(CFL(end)), ' for p = ', num2str(degree)]);
+end
+
+plot(ps, CFL, '*-')
+save('fdm','CFL')
