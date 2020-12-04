d91ec89198c39faf241e55c2f51305be52e2bef7
Zackeus Zetterberg <zackeua@gmail.com>
2020-12-04 14:50:56 +0100

    sparat figurer till redovisningen

--- C_effPlot.m
+++ C_effPlot.m
@@ -19,11 +19,12 @@ plot(C_gaussLobatto,'-*');
 plot(C_gaussLobattoExact,'-*');
 plot(2*(1:4),CFL,'-*');
 hold off;
-legend('Uniform', 'Gauss Lobatto', 'Gauss Lobatto exact integration', 'FDM');
-xlabel('Degree')
+legend('Lagrangian FEM - evenly spaced points with exact integration', 'Lagrangian FEM - GL points with GL quadrature', 'Lagrangian FEM - GL points with exact integration', 'FDM - with central difference scheme');
+xlabel({'Degree (FEM)', 'Order (FDM)'})
 ylabel('C_{eff}')
-title('Rescaled efficiency number as a function of the polynomial degree');
-
+title('Rescaled efficiency number for different numerical methods')
+%title({'Rescaled efficiency number as a function of the','polynomial degree for FEM and', 'order of the FDM central scheme'});
+set(gca,'FontSize',15)
 
 figure
 plot(1./C_uni,'-*');
@@ -32,7 +33,9 @@ plot(1./C_gaussLobatto,'-*');
 plot(1./C_gaussLobattoExact,'-*');
 plot(2*(1:4),1./CFL,'-*');
 hold off;
-legend('Uniform', 'Gauss Lobatto', 'Gauss Lobatto exact integration', 'FDM');
-xlabel('Degree')
-ylabel('1/C_{eff}')
-title('Rescaled efficiency number as a function of the polynomial degree');
+legend('Lagrangian FEM - evenly spaced points with exact integration', 'Lagrangian FEM - GL points with GL quadrature', 'Lagrangian FEM - GL points with exact integration', 'FDM - with central difference scheme');
+xlabel({'Degree (FEM)','Order (FDM)'})
+ylabel('Reciprocal of C_{eff}')
+title('Reciprocal rescaled efficiency number for different numerical methods')
+%title({'Reciprocal of rescaled efficiency number as a function of the','polynomial degree for FEM and', 'order of the FDM central scheme'});
+set(gca,'FontSize',15)
